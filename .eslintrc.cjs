03324eb59181450ebbbdafebbe07091a80fa59e2
songlh <song.lhlh@gmail.com>
2022-09-14 11:10:50 +0800

    chore: 修改eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
     'react/no-unknown-property': ['error', { ignore: ['t', 'p-id'] }]
