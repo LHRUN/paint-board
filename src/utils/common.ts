88b25adcfda42a83df351fc73da68b04e0fb487a
LH_R <song.lhlh@gmail.com>
2022-12-03 17:43:02 +0800

    feat: 修改历史记录逻辑，增加版本缓存数据兼容

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -168,3 +168,45 @@ export const createDocument = <T extends keyof HTMLElementTagNameMap>(
   parent.appendChild(el)
   return el
 }
+
+/**
+ * 获取数组元素，未传坐标时返回最后一个元素
+ * @param arr 数组
+ * @param n 坐标
+ */
+export const at = <T>(arr: T[], n?: number) => {
+  n = n ?? arr.length - 1
+  if (n < 0 || n >= arr.length) return undefined
+  return arr[n]
+}
+
+/**
+ * 比较版本号
+ * @param v1
+ * @param v2
+ * @returns 0: v1 === v2; 1: v1 > v2; -1: v1 < v2
+ */
+export const compareVersion = (v1: string, v2: string) => {
+  const v1s = v1.split('.')
+  const v2s = v2.split('.')
+  const len = Math.max(v1s.length, v2s.length)
+
+  while (v1s.length < len) {
+    v1s.push('0')
+  }
+  while (v2s.length < len) {
+    v1s.push('0')
+  }
+
+  for (let i = 0; i < len; i++) {
+    const num1 = parseInt(v1s[i])
+    const num2 = parseInt(v2s[i])
+
+    if (num1 > num2) {
+      return 1
+    } else if (num1 < num2) {
+      return -1
+    }
+  }
+  return 0
+}
--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,3 +1,9 @@
+import { ELEMENT_INSTANCE } from '@/types'
+import { cloneDeep } from 'lodash'
+import { at, compareVersion } from './common'
+import { CANVAS_ELE_TYPE } from './constants'
+import { calculateRect, FreeLine, initRect } from './element/freeLine'
+
 export enum EACH_ORDER_TYPE {
   FIRST = 'first', // 顺序
   LAST = 'last' // 倒序
@@ -7,25 +13,32 @@ export enum EACH_ORDER_TYPE {
  * 历史记录栈
  */
 export class History<T> {
-  cacheQueue: T[]
+  cacheStack: Array<T[]>
   step: number
-  constructor(cacheQueue: T[]) {
-    this.cacheQueue = cacheQueue
-    this.step = cacheQueue.length - 1
+  constructor(cacheStack: T[], version: string) {
+    console.log(cacheStack, version)
+    formatHistory(cacheStack, version)
+    this.cacheStack = [cacheStack]
+    this.step = 0
   }
 
   /**
    * 遍历cacheQueue
    * @param cb 遍历执行回调
+   * @param order 倒叙 | 正序
    */
   each(cb?: (ele: T, i: number) => void, order = EACH_ORDER_TYPE.FIRST) {
-    if (order === EACH_ORDER_TYPE.FIRST) {
-      for (let i = 0; i <= this.step; i++) {
-        cb?.(this.cacheQueue[i], i)
-      }
-    } else if (order === EACH_ORDER_TYPE.LAST) {
-      for (let i = this.step - 1; i >= 0; i--) {
-        cb?.(this.cacheQueue[i], i)
+    const cur = this.getCurrentStack()
+
+    if (cur) {
+      if (order === EACH_ORDER_TYPE.FIRST) {
+        for (let i = 0; i < cur.length; i++) {
+          cb?.(cur[i], i)
+        }
+      } else if (order === EACH_ORDER_TYPE.LAST) {
+        for (let i = cur.length - 1; i >= 0; i--) {
+          cb?.(cur[i], i)
+        }
       }
     }
   }
@@ -34,20 +47,24 @@ export class History<T> {
    * 对缓存进行排序
    */
   sort(cb: (a: T, b: T) => number) {
-    this.cacheQueue.sort(cb)
+    const last = at(this.cacheStack)
+    last?.sort(cb)
   }
 
   /**
    * 添加数据
-   * @param data
    */
   add(data: T) {
     // 如果在回退时添加数据就删除暂存数据
-    if (this.step !== this.cacheQueue.length - 1) {
-      this.cacheQueue.length = this.step + 1
+    if (this.step !== this.cacheStack.length - 1) {
+      this.cacheStack.length = this.step + 1
     }
-    this.cacheQueue.push(data)
-    this.step = this.cacheQueue.length - 1
+
+    const last = at(this.cacheStack)
+    console.log(last)
+    const newData = last ? [...cloneDeep(last), data] : [data]
+    this.cacheStack.push(newData)
+    this.step = this.cacheStack.length - 1
   }
 
   /**
@@ -56,7 +73,19 @@ export class History<T> {
    * @param value 删除条件匹配的值
    */
   delete<K extends keyof T>(key: K, value: T[K]) {
-    this.cacheQueue = this.cacheQueue.filter((item) => item[key] !== value)
+    if (this.step !== this.cacheStack.length - 1) {
+      this.cacheStack.length = this.step + 1
+    }
+    const last = cloneDeep(at(this.cacheStack)) as T[]
+    const newData =
+      last?.filter((item) => {
+        if (item && Object.hasOwn(item, key)) {
+          return item[key] !== value
+        }
+        return false
+      }) ?? []
+    this.cacheStack.push(newData)
+    this.step = this.cacheStack.length - 1
   }
 
   /**
@@ -64,7 +93,13 @@ export class History<T> {
    * @param index 下标
    */
   deleteByIndex(index: number) {
-    this.cacheQueue.splice(index, 1)
+    if (this.step !== this.cacheStack.length - 1) {
+      this.cacheStack.length = this.step + 1
+    }
+    const newData = cloneDeep(at(this.cacheStack)) as T[]
+    newData.splice(index, 1)
+    this.cacheStack.push(newData)
+    this.step = this.cacheStack.length - 1
   }
 
   /**
@@ -73,7 +108,7 @@ export class History<T> {
   undo() {
     if (this.step >= 0) {
       this.step--
-      return this.cacheQueue[this.step]
+      return this.cacheStack[this.step]
     }
   }
 
@@ -81,9 +116,9 @@ export class History<T> {
    * 前进
    */
   redo() {
-    if (this.step < this.cacheQueue.length - 1) {
+    if (this.step < this.cacheStack.length - 1) {
       this.step++
-      return this.cacheQueue[this.step]
+      return this.cacheStack[this.step]
     }
   }
 
@@ -91,7 +126,49 @@ export class History<T> {
    * 清空
    */
   clean() {
-    this.cacheQueue = []
+    this.cacheStack = []
     this.step = -1
+  }
+
+  /**
+   * 获取当前层
+   */
+  getCurrentStack() {
+    return at(this.cacheStack, this.step < 0 ? 0 : this.step) as T[]
+  }
+
+  /**
+   * 缓存栈插入数据
+   * @param newData 插入数据
+   * @param replaceData 需替换的数据
+   * @returns 缓存栈
+   */
+  pushStack(newData: T[], replaceData?: T[]) {
+    if (this.step !== this.cacheStack.length - 1) {
+      this.cacheStack.length = this.step + 1
+    }
+    this.cacheStack.push(cloneDeep(newData))
+    if (replaceData) {
+      this.cacheStack[this.cacheStack.length - 2] = cloneDeep(replaceData)
+    }
+    this.step = this.cacheStack.length - 1
+    return this.cacheStack
+  }
+}
+
+/**
+ * 处理历史记录栈格式，主要用于版本兼容
+ * @param stack
+ */
+export const formatHistory = (stack: ELEMENT_INSTANCE[], version: string) => {
+  if (compareVersion(version, '0.1.1') < 0) {
+    stack.forEach((ele) => {
+      if (ele.type === CANVAS_ELE_TYPE.FREE_LINE) {
+        initRect(ele as FreeLine)
+        ;(ele as FreeLine).positions.forEach((position) => {
+          calculateRect(ele as FreeLine, position)
+        })
+      }
+    })
   }
 }
