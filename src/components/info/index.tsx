538db3abaee681dbe8546e51413f03d8854b2a55
LH_R <song.lhlh@gmail.com>
2023-02-16 21:55:16 +0800

    feat: add i18n cache

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -4,15 +4,19 @@ import InfoIcon from '@/components/icons/info'
 import Mask from '@/components/mask'
 import ZhIcon from '../icons/zh'
 import EnIcon from '../icons/en'
+import { BOARD_LOCAL_KEY, storage } from '@/utils/storage'
+import styles from './index.module.css'
 
 /**
  * 操作指南弹窗
  */
 const Info: React.FC = () => {
-  const { i18n } = useTranslation()
+  const { i18n, t } = useTranslation()
   const [showModal, setShowModal] = useState<boolean>(false)
   const handleChangLang = () => {
-    i18n.changeLanguage(i18n.language === 'en' ? 'zh' : 'en')
+    const language = i18n.language === 'en' ? 'zh' : 'en'
+    storage.set(BOARD_LOCAL_KEY, language)
+    i18n.changeLanguage(language)
   }
 
   return (
@@ -29,48 +33,26 @@ const Info: React.FC = () => {
           setShowModal(false)
         }}
       >
-        <div className="flex flex-col bg-white rounded-2xl overflow-hidden">
-          <div className="font-bold my-4 mx-4">
-            repo:
-            <a
-              className="link link-primary inline-block ml-1 mr-3"
-              href="https://github.com/LHRUN/paint-board.git"
-              target="_blank"
-              rel="noreferrer"
-            >
-              https://github.com/LHRUN/paint-board.git
-            </a>
-            欢迎Star⭐️
-            {/* <label className="swap">
-              <input type="checkbox" />
-              <span className="swap-on fill-current w-10 h-10">
-                <ZhIcon></ZhIcon>
-              </span>
-              <span className="swap-off fill-current w-10 h-10">
-                <EnIcon></EnIcon>
-              </span>
-            </label> */}
-            <span className="cursor-pointer" onClick={handleChangLang}>
+        <div className="flex flex-col bg-white rounded-2xl overflow-hidden w-7/12 py-1 px-3">
+          <div className="flex items-center justify-between font-bold my-4 mx-4">
+            <div>
+              repo:
+              <a
+                className="link link-primary inline-block ml-1 mr-3 break-all"
+                href="https://github.com/LHRUN/paint-board.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://github.com/LHRUN/paint-board.git
+              </a>
+              {t('info.welecome')}⭐️
+            </div>
+            <span className={styles.i18nBtn} onClick={handleChangLang}>
               {i18n.language === 'en' ? <EnIcon /> : <ZhIcon />}
             </span>
-            {/* <span>
-              <label className="cursor-pointer label">
-                <span className="label-text">ZH</span>
-                <input
-                  type="checkbox"
-                  className="toggle"
-                  onChange={handleChangLang}
-                  checked={i18n.language === 'en'}
-                />
-                <span className="label-text">EN</span>
-              </label>
-            </span> */}
           </div>
 
-          <img
-            className="mask w-100"
-            src="https://s1.ax1x.com/2022/12/17/zHbHJK.png"
-          />
+          <img className="mask" src={t('info.url') || ''} />
         </div>
       </Mask>
     </>
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,9 +2,10 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from './en.json'
 import zh from './zh.json'
+import { BOARD_LOCAL_KEY, storage } from '@/utils/storage'
 
 const lang =
-  localStorage.getItem('local') ||
+  storage.get(BOARD_LOCAL_KEY) ||
   (['en', 'en-US', 'en-us'].includes(navigator.language) ? 'en' : 'zh')
 
 i18n.use(initReactI18next).init({
@@ -16,7 +17,7 @@ i18n.use(initReactI18next).init({
       translation: zh
     }
   },
-  lng: 'en'
+  lng: lang
 })
 
 export default i18n
